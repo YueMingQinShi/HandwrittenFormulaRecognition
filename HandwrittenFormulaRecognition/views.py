f044e75061a49b91fe9b0850748e59d3d953b058
pzq <pzq797933@163.com>
2019-07-01 11:14:21 +0800

    format + backup

--- HandwrittenFormulaRecognition/views.py
+++ HandwrittenFormulaRecognition/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'index.html')
 
+
 def upload(request):
     return render(request, 'upload.html')
 
-def handwrite(request):
-    return render(request, 'handwrite.html')
+
+def hand_write(request):
+    return render(request, 'hand_write.html')
